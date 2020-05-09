4a5ce9f8f771c28fd960efccbd65f5b9bcee68a3
manisha <guptamanisha3845@gmail.com>
2020-05-09 15:29:55 +0530

    Pipeline View3

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>WELCOME</h1>
 
 <?php
+$color = "red";
+echo "ManishaGupta " . $color . "<br>";
 print "Welcome To my php page";
 print " Have a good day ";
 print "Regards Manisha with love";
 print "Ok Byeeeeeeeeeeeeeeeeeeeee";
 ?>
+
+
+</body>
+</html>
+
